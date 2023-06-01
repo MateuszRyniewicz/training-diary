e696e53b6e0fbb4c300e23d8f97afa40fcfae29b
Mateusz <ryniuss@o2.pl>
2023-06-01 19:02:24 +0200

    TD12: create dynamically add and remove input

--- src/Pages/NewTrainigPage.js
+++ src/Pages/NewTrainigPage.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { Children } from 'react';
+import { useState } from 'react';
 
 const NewTrainigPage = () => {
-	return <div>NewTrainigPage formularz dynamicznie plus i minus</div>;
+	const [inputFields, setInputFields] = useState([]);
+
+	const addInputFields = () => {
+		setInputFields([...inputFields, []]);
+	};
+
+	const changeInputValue = (e, i) => {
+		const inputData = [...inputFields];
+		inputData[i] = e.target.value;
+		setInputFields(inputData);
+		// setInputFields([...(inputFields[i] = e.target.value)]);
+	};
+
+	const deleteInput = (i) => {
+		const deleteInput = [...inputFields];
+		deleteInput.splice(i, 1);
+		setInputFields(deleteInput);
+	};
+
+	console.log(inputFields);
+
+	return (
+		<>
+			<button onClick={() => setInputFields([...inputFields, []])}>Add</button>
+			{inputFields.map((newInput, i) => {
+				return (
+					<div>
+						<input value={newInput} onChange={(e) => changeInputValue(e, i)} />
+						<button onClick={() => deleteInput(i)}>X</button>
+					</div>
+				);
+			})}
+		</>
+	);
 };
 
 export default NewTrainigPage;
